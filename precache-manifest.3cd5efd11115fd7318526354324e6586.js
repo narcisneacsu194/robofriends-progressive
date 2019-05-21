f84897fd742ad1b09ac973a701dff9195428bd2c
narcisneacsu194 <narcisneacsu194@gmail.com>
2019-05-22 01:55:40 +0300

    Updates

---
+++ precache-manifest.3cd5efd11115fd7318526354324e6586.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23f778ace237f1c8642cdcdb0133996e",
+    "url": "/robofriends-progressive/index.html"
+  },
+  {
+    "revision": "d7255c7197d45c8925ad",
+    "url": "/robofriends-progressive/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "3f6fd220a578e6198d59",
+    "url": "/robofriends-progressive/static/css/main.f35ac674.chunk.css"
+  },
+  {
+    "revision": "d7255c7197d45c8925ad",
+    "url": "/robofriends-progressive/static/js/2.132c45fb.chunk.js"
+  },
+  {
+    "revision": "3f6fd220a578e6198d59",
+    "url": "/robofriends-progressive/static/js/main.3e610eb4.chunk.js"
+  },
+  {
+    "revision": "63b26d129910fabec72b",
+    "url": "/robofriends-progressive/static/js/runtime~main.be234e85.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-progressive/static/media/SEGA.12a3db8b.woff"
+  }
+]);
