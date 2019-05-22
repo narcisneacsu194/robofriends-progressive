24600452635f58e93689d6a48473892ed4e6e8c6
narcisneacsu194 <narcisneacsu194@gmail.com>
2019-05-22 21:24:57 +0300

    Made the application progressive.

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,12 +4,7 @@ const Card = props => {
   const { id, name, email } = props;
   return (
     <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-      <img
-        alt="robots"
-        src={`https://robohash.org/${id}`}
-        width="200"
-        height="200"
-      />
+      <img alt="robots" src={`https://robohash.org/${id}?size=200x200`} />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
